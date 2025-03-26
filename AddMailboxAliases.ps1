661d932e7ee803c3b486974857abc672d8b66c8a
Khalil <khalilftw0@gmail.com>
2025-03-26 16:57:21 +0000

    Module Check and connecting to Exchange sorted

--- AddMailboxAliases.ps1
+++ AddMailboxAliases.ps1
@@ -1,4 +1,36 @@
-# Adding 100 Aliases to an account
+# AddMailboxAliases - Script by Khalil
+# Used to add Multiple Aliases to an Email Account
+
+function WriteR($message) { Write-Host -ForegroundColor "Red" $message }
+function WriteG($message) { Write-Host -ForegroundColor "Green" $message }
+function WriteY($message) { Write-Host -ForegroundColor "Yellow" $message }
+
+# Module Check to ensure Exchange is installed
+$Modules = @("ExchangePowerShell","ExchangeOnlineManagement")
+try {
+    foreach ($Module in $Modules) {
+        if ($Module -notin (Get-InstalledModule).Name) {
+            Write-Host "Installing $Module"
+            Install-Module -Name $Module -Force | Out-Null
+        }
+    }
+} catch [Exception] {
+    Write-Host "Couldn't install Powershell Modules"
+    Read-Host
+    $_ # This will rarely happen so an error output would be useful
+    exit
+}
+
+# Connecting to Exchange
+try {
+    Connect-ExchangeOnline -ErrorAction Stop | Out-Null
+} catch {
+    WriteR "Couldn't connect to Exchange"
+    Read-Host
+    exit
+}
+
+# continue with rest of the stuff
 
 $array = @()
 foreach ($num in 1..100) {
